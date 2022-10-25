8d3c094d632c8543cf9eb8110d9b13629963a6b6
Sam Muir <samhmuir@Sams-MacBook-Air.local>
2022-10-25 15:30:29 -0400

    data cleaning

--- app/app.R
+++
@@ -1,8 +0,0 @@
-# Load data and libraries -------------------------------------------
-library("shiny")
-source("ui.R")
-source("server.R")
-source("weather.R")
-
-# Create shiny application ------------------------------------------
-shinyApp(ui = ui, server = server)
